c40c25151ef366dcb4cf7a6b6d9bd998bdb6d4bd
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-12-03 12:07:12 +0400

    fix: updated agreements setup

--- src/pages/UserAgreements/index.jsx
+++ src/pages/UserAgreements/index.jsx
@@ -68,18 +68,19 @@ const UserAgreements = () => {
         id: agreement.id,
         status: STATUS_MAPPING[agreement.status] || agreement.status,
         created_at: agreement.created_at,
-        name: agreement.contragent_name || "N/A",
         price: agreement.product_cost,
-        payment_terms: agreement.product_payment_term,
-        buyer: agreement.buyer_name + " " + agreement.buyer_surname,
-        file_path: "/storage/app/templates/agreement_template_1.docx",
         contragent: {
           name: agreement.contragent_name,
           address: agreement.contragent_address,
           phone: agreement.contragent_phone_number,
           email: agreement.contragent_email,
-          director: agreement.contragent_director,
         },
+        director: {
+          name: agreement.contragent_director_name,
+          phone: agreement.contragent_director_phone_number,
+        },
+        different_terms: agreement.payment_different_terms,
+        contract_initiator: agreement.contract_initiator_name,
       }
     })
   }, [agreements])
@@ -91,28 +92,28 @@ const UserAgreements = () => {
         accessor: "id",
       },
       {
-        Header: "კონტრაგენტის სახელი",
+        Header: "კონტრაგენტის დასახელება/სახელი და გვარი",
         accessor: "contragent.name",
       },
       {
-        Header: "კონტრაგენტის მისამართი",
+        Header: "მისამართი",
         accessor: "contragent.address",
       },
       {
-        Header: "კონტრაგენტის ნომერი",
+        Header: "ტელეფონის ნომერი",
         accessor: "contragent.phone",
       },
       {
-        Header: "კონტრაგენტის ელ-ფოსტა",
+        Header: "ელ.ფოსტა",
         accessor: "contragent.email",
       },
       {
-        Header: "მყიდველი",
-        accessor: "buyer",
+        Header: "დირექტორის სახელი და გვარი",
+        accessor: "director.name",
       },
       {
-        Header: "დირექტორი",
-        accessor: "contragent.director",
+        Header: "დირექტორის ტელეფონის ნომერი",
+        accessor: "director.phone",
       },
       {
         Header: "პროდუქციის ღირებულება",
@@ -189,12 +190,30 @@ const UserAgreements = () => {
 
   const renderRowDetails = useCallback(row => {
     if (!row) {
-      console.error("Row data is missing")
       return null
     }
 
     return (
       <div className="p-3">
+        <div className="mb-3">
+          <Row>
+            <Col md={6}>
+              <div className="mb-2">
+                <strong> გადახდის განსხვავებული პირობები: </strong>
+                {row.payment_different_terms ? "კი" : "არა"}
+              </div>
+            </Col>
+            <Col md={6}>
+              <div className="mb-2">
+                <strong>
+                  ხელშეკრულების გაფორმების ინიციატორი და შესრულებაზე
+                  პასუხისმგებელი პირი:{" "}
+                </strong>
+                {row.contract_initiator}
+              </div>
+            </Col>
+          </Row>
+        </div>
         <button
           className="btn btn-primary btn-sm"
           onClick={() => handleDownload(row.id)}
